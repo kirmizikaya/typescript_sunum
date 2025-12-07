c2c9028abb36aa15571c391c62f4acffb6a69f39
Mehmet Kırmızıkaya <mehmet.kirmizikaya@emlakjet.com>
2025-12-08 00:56:50 +0300

    nedir bu extends kavrami

--- app/sunum/NedirBuExtendsKavrami/page.tsx
+++ app/sunum/NedirBuExtendsKavrami/page.tsx
@@ -41,3 +41,80 @@ const Sayfa12 = <T,>({ items }: { items: T[] }) => {
 
 export default Sayfa12;
 
+
+
+
+//// Not: Aşağıdaki kodlar sadece örnej amaçlıdır. Yukarıdaki kodun parçası değildir.
+
+// Interface extension
+interface Canli {
+    yasıyor: boolean;
+    yas: number;
+}
+
+interface Hayvan extends Canli {
+    tur: string;
+    beslenme: "otobur" | "etobur" | "hepciyor";
+}
+
+interface Memeli extends Hayvan {
+    kanSicakligi: "sicakKanli" | "soğukKanli";
+    emzirme: boolean;
+}
+
+// Çoklu interface extension
+interface Ucabilen {
+    kanatAcikligi: number;
+    ucabilir: true;
+}
+
+interface Kus extends Memeli, Ucabilen {
+    tuyRengi: string;
+}
+
+const kartal: Kus = {
+    yasıyor: true,
+    yas: 5,
+    tur: "Kartal",
+    beslenme: "etobur",
+    kanatAcikligi: 200,
+    ucabilir: true,
+    tuyRengi: "kahverengi",
+    kanSicakligi: "soğukKanli",
+    emzirme: true
+};
+
+
+//Type Intersection ile Birleştirme
+// Type intersection (&) ile birleştirme
+type Kimlik = {
+    id: string;
+    olusturulmaTarihi: Date;
+};
+
+type KisiselBilgi = {
+    ad: string;
+    soyad: string;
+    dogumTarihi: Date;
+};
+
+type IletisimBilgisi = {
+    email: string;
+    telefon: string;
+    adres?: string;
+};
+
+// Tüm tipleri birleştir
+type TamKullanici = Kimlik & KisiselBilgi & IletisimBilgisi;
+
+const kullanici: TamKullanici = {
+    id: "usr-123",
+    olusturulmaTarihi: new Date(),
+    ad: "Mehmet",
+    soyad: "Yıldız",
+    dogumTarihi: new Date("1990-05-15"),
+    email: "mehmet@example.com",
+    telefon: "5551234567"
+};
+
+
